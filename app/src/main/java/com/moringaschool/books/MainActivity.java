0ef819d68a4dfc3e46935a85dd0d4dc7b9da3302
brendahuwitonze <uwitonzebrendah@gmail.com>
2021-02-04 23:22:52 +0200

    added create account activty

--- app/src/main/java/com/moringaschool/books/MainActivity.java
+++ app/src/main/java/com/moringaschool/books/MainActivity.java
@@ -6,27 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-//
-//public class MainActivity extends AppCompatActivity {
-//    @BindView(R.id.button)
-//    Button b1;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
-//
-//        b1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this,BookListActivity.class);
-//                startActivity(intent);
-//
-//            }
-//        });
-//    }
-//}
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.button) Button b1;
+    @BindView(R.id.text1) EditText Text1;
+    @BindView(R.id.text2) EditText Text2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,BookListActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
