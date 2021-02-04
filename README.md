9e2f0d52506f2c0197f06ca3d91b85ae17bab73c
brendahuwitonze <uwitonzebrendah@gmail.com>
2021-02-04 12:49:25 +0200

    added content to the readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# BLIBRARY APPLICATION
+## [BOOKS](https://github.com/brendahuwitonze/BOOKS)
+
+### AUTHOR:UWITONZEBRENDAH
+
+This application will allow users to recommend books to read to friends 
+
+##### BDD
+* user input the username user enters the password then click login
+
+LANGUAGE USED
+
+Android studio Java
+
+### LINCENSE
+This project is licensed under the terms of the MIT license. Copyright (c) 2020 UWITONZEBRENDA
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
--- app/src/main/java/com/moringaschool/books/MainActivity.java
+++ app/src/main/java/com/moringaschool/books/MainActivity.java
@@ -9,24 +9,24 @@ import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.button)
-    Button b1;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,BookListActivity.class);
-                startActivity(intent);
-
-            }
-        });
-    }
-}
+//
+//public class MainActivity extends AppCompatActivity {
+//    @BindView(R.id.button)
+//    Button b1;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        ButterKnife.bind(this);
+//
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this,BookListActivity.class);
+//                startActivity(intent);
+//
+//            }
+//        });
+//    }
+//}
