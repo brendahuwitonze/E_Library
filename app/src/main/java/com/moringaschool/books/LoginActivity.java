29ceca9dd604c5c9b2f358cb7ddbd7331ac7790c
brendahuwitonze <uwitonzebrendah@gmail.com>
2021-02-05 09:16:23 +0200

    made the login activity the Launcher

--- app/src/main/java/com/moringaschool/books/LoginActivity.java
+++ app/src/main/java/com/moringaschool/books/LoginActivity.java
@@ -2,13 +2,43 @@ package com.moringaschool.books;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.passwordLoginButton)
+    TextView mRegisterTextView;
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+        mRegisterTextView2.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, BookListActivity.class);
+            startActivity(intent);
+            finish();
+        }
+       else if (view == mRegisterTextView2) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+
 }
